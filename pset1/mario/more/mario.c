54735d13258f19f4ce7dedf89171f647295bd99c
Sitora Urazova <sitoraurazova@gmail.com>
2018-11-27 00:09:05 +0000

    Added Mario

--- pset1/mario/more/mario.c
+++ pset1/mario/more/mario.c
@@ -2,5 +2,26 @@
 
 int main(void)
 {
-   
+   int height;
+
+    do
+    {
+        printf("All the height of your pyramid= ");
+        height = GetInt();
+    }
+    while (height < 0 || height > 23);
+
+    //add for loop 
+    for (int line = 0; line < height; line++)
+    {
+        for (int spaces = height - line; spaces > 1; spaces--)
+        {
+            printf(" ");
+        }
+        for (int hashes = 0; hashes < line + 2; hashes++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
